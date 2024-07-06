ec872ce3e82a00e0637b30878856a7d0265c8bce
Jerrika Gabriel <jerrikagabriel@Jerrikas-Air.attlocal.net>
2024-07-05 20:05:48 -0500

    installed packages and connect to Mongo DB.

--- Server.js
+++ Server.js
@@ -9,3 +9,19 @@ app.use(express.json());
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
