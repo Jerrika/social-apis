c023007cd27fd1da8965c88d16fe759bd254dda7
Jerrika Gabriel <jerrikagabriel@Jerrikas-Air.attlocal.net>
2024-07-05 20:19:46 -0500

    added model folders and files, and route/api folders.

---
+++ models/thought.js
@@ -0,0 +1,22 @@
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: timestamp => dateFormat(timestamp)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
